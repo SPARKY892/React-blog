e64f65949d132483347b9872ae80a1c62ae9c3d6
Mark <marksmellie892@gmail.com>
2020-08-13 17:48:05 +0100

    cleaned up white space

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,8 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 class UserHeader extends React.Component {
-
-
     render() {
         const {user} = this.props;
         if(!user) {
